455153033c059e445c527d7d0250c4acf5332551
avotayruk <kuratoann@gmail.com>
2026-03-31 11:30:25 +0300

    added lab3 files (my bad)

--- Core/Src/platform/platform.c
+++ Core/Src/platform/platform.c
@@ -1,15 +1,24 @@
 /* Директива препроцессора для подключения заголовочного файла */
 #include "platform.h"
 
-//#include "lab1_gpio/lab1_gpio.h"
-//#include "lab2_timer/lab2_timer.h"
-//#include "lab3_stepper/lab3_stepper.h"
+#include "lab1_gpio/lab1_gpio.h"
+#include "lab2_timer/lab2_timer.h"
+#include "lab3_stepper/lab3_stepper.h"
+
 
 int run = 1;
-	int pause = 20;
-	int b = 0;
-	int dir = 1;
+int pause = 20;
+int b = 0;
+int dir = 1;
+
 /* Однократный вызов */
+
+//void my_function(void) {
+//	    static int step_n = 0;  // инициализируется один раз
+//	    step_n++;
+//	    printf("Шаг: %d\n", step_n);
+//	}
+
 int plt_init(void)
 {
 
@@ -17,12 +26,22 @@ int plt_init(void)
 }
 
 
+//	int step_n = 0;
+	int half_step_n = 0;
+
 /* Повторяющийся вызов */
+
 void plt_process(void)
 {
-	led_brightness(b);
-	b = b + dir;
-	if(b >= 100 || b <= 0) dir = dir*(-1);
+//
+//	plt_stepper_full(step_n);
+//	step_n = (step_n+1)%4;
+//	plt_delay(20);
+
+	plt_stepper_half(half_step_n);
+		half_step_n = (half_step_n)%8;
+		plt_delay(10);
+// это для полушагового
 }
 
 /* Перенести в main.c в user code */
